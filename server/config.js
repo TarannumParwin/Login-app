3ec78bac1ba5bf5e839cee1df776667c11b84ebb
tarannumparwin <tarannumparwin1995@gmail.com>
2023-04-05 14:51:17 +0530

    [Tara MERN Project]

---
+++ server/config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "<secret>",
+    EMAIL: "steve.franecki@ethereal.email", // testing email & password
+    PASSWORD : "sMf46xCzrvdrxvuagc",
+    ATLAS_URI: "mongodb+srv://tarannumparwin1111:tarannumparwin1111@cluster0.qih2m6h.mongodb.net/?retryWrites=true&w=majority"
+}
